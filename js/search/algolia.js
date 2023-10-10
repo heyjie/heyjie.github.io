bac5c92b6595e73932ba99d6069dccf9d6302b06
heyjie <yingjiex.he@intel.com>
2023-10-10 10:58:15 +0800

    Site updated: 2023-10-10 10:58:07

--- js/search/algolia.js
+++ js/search/algolia.js
@@ -1,344 +1,163 @@
-var SearchService = '';
+window.addEventListener('load', () => {
+  const openSearch = () => {
+    const bodyStyle = document.body.style
+    bodyStyle.width = '100%'
+    bodyStyle.overflow = 'hidden'
+    acy.animateIn(document.getElementById('search-mask'), 'to_show 0.5s')
+    acy.animateIn(document.querySelector('#algolia-search .search-dialog'), 'titleScale 0.5s')
+    setTimeout(() => { document.querySelector('#algolia-search .ais-SearchBox-input').focus() }, 100)
 
-(function($) {
+    // shortcut: ESC
+    document.addEventListener('keydown', function f (event) {
+      if (event.code === 'Escape') {
+        closeSearch()
+        document.removeEventListener('keydown', f)
+      }
+    })
+  }
 
-  /**
-   * A super class of common logics for all search services
-   * @param options : (object)
-   */
-  SearchService = function(options) {
-    var self = this;
+  const closeSearch = () => {
+    const bodyStyle = document.body.style
+    bodyStyle.width = ''
+    bodyStyle.overflow = ''
+    acy.animateOut(document.querySelector('#algolia-search .search-dialog'), 'search_close .5s')
+    acy.animateOut(document.getElementById('search-mask'), 'to_hide 0.5s')
+  }
 
-    self.config = $.extend({
-      per_page : 10,
-      selectors: {
-        body              : 'body',
-        form              : '.u-search-form',
-        input             : '.u-search-input',
-        container         : '#u-search',
-        modal             : '#u-search .modal',
-        modal_body        : '#u-search .modal-body',
-        modal_footer      : '#u-search .modal-footer',
-        modal_overlay     : '#u-search .modal-overlay',
-        modal_results     : '#u-search .modal-results',
-        modal_metadata    : '#u-search .modal-metadata',
-        modal_error       : '#u-search .modal-error',
-        modal_loading_bar : '#u-search .modal-loading-bar',
-        modal_ajax_content: '#u-search .modal-ajax-content',
-        modal_logo        : '#u-search .modal-footer .logo',
-        btn_close         : '#u-search .btn-close',
-        btn_next          : '#u-search .btn-next',
-        btn_prev          : '#u-search .btn-prev'
+  const searchClickFn = () => {
+    document.querySelector('#search-button > .search').addEventListener('click', openSearch)
+  }
+
+  const searchClickFnOnce = () => {
+    document.getElementById('search-mask').addEventListener('click', closeSearch)
+    document.querySelector('#algolia-search .search-close-button').addEventListener('click', closeSearch)
+  }
+
+  const cutContent = content => {
+    if (content === '') return ''
+
+    const firstOccur = content.indexOf('<mark>')
+
+    let start = firstOccur - 30
+    let end = firstOccur + 120
+    let pre = ''
+    let post = ''
+
+    if (start <= 0) {
+      start = 0
+      end = 140
+    } else {
+      pre = '...'
+    }
+
+    if (end > content.length) {
+      end = content.length
+    } else {
+      post = '...'
+    }
+
+    let matchContent = pre + content.substring(start, end) + post
+    return matchContent
+  }
+
+  const algolia = GLOBAL_CONFIG.algolia
+  const isAlgoliaValid = algolia.appId && algolia.apiKey && algolia.indexName
+  if (!isAlgoliaValid) {
+    return console.error('Algolia setting is invalid!')
+  }
+
+  const search = instantsearch({
+    indexName: algolia.indexName,
+    searchClient: algoliasearch(algolia.appId, algolia.apiKey),
+    searchFunction(helper) {
+      helper.state.query && helper.search()
+    },
+  })
+
+  const configure = instantsearch.widgets.configure({
+    hitsPerPage: 5
+  })
+
+  const searchBox = instantsearch.widgets.searchBox({
+    container: '#algolia-search-input',
+    showReset: false,
+    showSubmit: false,
+    placeholder: GLOBAL_CONFIG.algolia.languages.input_placeholder,
+    showLoadingIndicator: true
+  })
+
+  const hits = instantsearch.widgets.hits({
+    container: '#algolia-hits',
+    templates: {
+      item(data) {
+        const link = data.permalink ? data.permalink : (GLOBAL_CONFIG.root + data.path)
+        const result = data._highlightResult
+        const content = result.contentStripTruncate
+                        ? cutContent(result.contentStripTruncate.value)
+                        : result.contentStrip
+                        ? cutContent(result.contentStrip.value)
+                        : result.content
+                        ? cutContent(result.content.value)
+                        : ''
+        return `
+          <a href="${link}" class="algolia-hit-item-link">
+          ${result.title.value || 'no-title'}
+          </a>
+          <p class="algolia-hit-item-content">${content}</p>`
       },
-      brands: {
-        'hexo'   : {logo: '', url: ''},
-        'google' : {logo: 'google.svg', url: 'https://cse.google.com'},
-        'algolia': {logo: 'algolia.svg', url: 'https://www.algolia.com'},
-        'baidu'  : {logo: 'baidu.svg', url: 'http://zn.baidu.com/cse/home/index'},
-        'azure'  : {logo: 'azure.svg', url: 'https://azure.microsoft.com/en-us/services/search/'}
-      },
-      imagePath: 'https://cdn.jsdelivr.net/gh/volantis-x/cdn-volantis@master/img/logo/'
-    }, options);
-
-    self.dom = {};
-    self.percentLoaded = 0;
-    self.open = false;
-    self.queryText = '';
-    self.nav = {
-      next   : -1,
-      prev   : -1,
-      total  : 0,
-      current: 1
-    };
-
-    self.parseSelectors = function() {
-      for (var key in self.config.selectors) {
-        self.dom[key] = $(self.config.selectors[key]);
+      empty: function (data) {
+        return (
+          '<div id="algolia-hits-empty">' +
+          GLOBAL_CONFIG.algolia.languages.hits_empty.replace(/\$\{query}/, data.query) +
+          '</div>'
+        )
       }
-    };
+    }
+  })
 
-    self.beforeQuery = function() {
-      if (!self.open) {
-        self.dom.container.fadeIn();
-        // self.dom.body.addClass('modal-active');
-        // 上面的是去除了文章的滚动条，我觉得没必要
+  const stats = instantsearch.widgets.stats({
+    container: '#algolia-info > .algolia-stats',
+    templates: {
+      text: function (data) {
+        const stats = GLOBAL_CONFIG.algolia.languages.hits_stats
+          .replace(/\$\{hits}/, data.nbHits)
+          .replace(/\$\{time}/, data.processingTimeMS)
+        return (
+          `<hr>${stats}`
+        )
       }
-      self.dom.input.each(function(index, elem) {
-        $(elem).val(self.queryText);
-      });
-      document.activeElement.blur();
-      self.dom.modal_error.hide();
-      self.dom.modal_ajax_content.removeClass('loaded');
-      self.startLoading();
-    };
+    }
+  })
 
-    self.afterQuery = function() {
-      self.dom.modal_body.scrollTop(0);
-      self.dom.modal_ajax_content.addClass('loaded');
-      self.stopLoading();
-    };
+  const powerBy = instantsearch.widgets.poweredBy({
+    container: '#algolia-info > .algolia-poweredBy',
+  })
 
-    /**
-     * Perform a complete serach operation including UI updates and query
-     * @param startIndex {int} start index or page number
-     */
-    self.search = function(startIndex, callback) {
-      self.beforeQuery();
-      if (self.search instanceof Function) {
-        self.query(self.queryText, startIndex, function() {
-          self.afterQuery();
-        });
-      } else {
-        console.log('query() does not exist.');
-        self.onQueryError(self.queryText, '');
-        self.afterQuery();
-      }
-    };
+  const pagination = instantsearch.widgets.pagination({
+    container: '#algolia-pagination',
+    totalPages: 5,
+    templates: {
+      first: '<i class="fas fa-angle-double-left"></i>',
+      last: '<i class="fas fa-angle-double-right"></i>',
+      previous: '<i class="fas fa-angle-left"></i>',
+      next: '<i class="fas fa-angle-right"></i>'
+    }
+  })
 
-    /**
-     * Query error handler
-     * @param queryText: (string)
-     * @param status: (string)
-     */
-    self.onQueryError = function(queryText, status) {
-      var errMsg = '';
-      if (status === 'success') errMsg = 'No result found for "' + queryText + '".';
-      else if (status === 'timeout') errMsg = 'Unfortunate timeout.';
-      else errMsg = 'Mysterious failure.';
-      self.dom.modal_results.html('');
-      self.dom.modal_error.html(errMsg);
-      self.dom.modal_error.show();
-    };
 
-    self.nextPage = function() {
-      if (self.nav.next !== -1) {
-        self.search(self.nav.next);
-      }
-    };
+  search.addWidgets([configure,searchBox,hits,stats,powerBy,pagination]) // add the widgets to the instantsearch instance
 
-    self.prevPage = function() {
-      if (self.nav.prev !== -1) {
-        self.search(self.nav.prev);
-      }
-    };
+  search.start()
 
-    self.getUrlRelativePath = function(url) {
-      var arrUrl = url.split('//');
-      var start = arrUrl[1].indexOf('/');
-      var relUrl = arrUrl[1].substring(start);
-      if (relUrl.indexOf('?') != -1) {
-        relUrl = relUrl.split('?')[0];
-      }
-      return relUrl;
-    };
+  searchClickFn()
+  searchClickFnOnce()
 
-    /**
-     * Generate html for one result
-     * @param url : (string) url
-     * @param title : (string) title
-     * @param digest : (string) digest
-     */
-    self.buildResult = function(url, title, digest) {
-      var result = self.getUrlRelativePath(url);
-      var html = '';
-      html = '<li>';
-      html += '<a class=\'result\' href=\'' + result + '\'>';
-      html += '<span class=\'title\'>' + title + '</span>';
-      if (digest !== '') html += '<span class=\'digest\'>' + digest + '</span>';
-      html += '</a>';
-      html += '</li>';
-      return html;
-    };
+  window.addEventListener('pjax:complete', () => {
+    getComputedStyle(document.querySelector('#algolia-search .search-dialog')).display === 'block' && closeSearch()
+    searchClickFn()
+  })
 
-    /**
-     * Close the modal, resume body scrolling
-     * no param
-     */
-    self.close = function() {
-      self.open = false;
-      self.dom.container.fadeOut();
-      self.dom.body.removeClass('modal-active');
-    };
-
-    /**
-     * Searchform submit event handler
-     * @param queryText : (string) the query text
-     */
-    self.onSubmit = function(event) {
-      event.preventDefault();
-      self.queryText = $(this).find('.u-search-input').val();
-      if (self.queryText) {
-        self.search(1);
-      }
-    };
-
-    /**
-     * Start loading bar animation
-     * no param
-     */
-    self.startLoading = function() {
-      self.dom.modal_loading_bar.show();
-      self.loadingTimer = setInterval(function() {
-        self.percentLoaded = Math.min(self.percentLoaded + 5, 95);
-        self.dom.modal_loading_bar.css('width', self.percentLoaded + '%');
-      }, 100);
-    };
-
-    /**
-     * Stop loading bar animation
-     * no param
-     */
-    self.stopLoading = function() {
-      clearInterval(self.loadingTimer);
-      self.dom.modal_loading_bar.css('width', '100%');
-      self.dom.modal_loading_bar.fadeOut();
-      setTimeout(function() {
-        self.percentLoaded = 0;
-        self.dom.modal_loading_bar.css('width', '0%');
-      }, 300);
-    };
-
-    /**
-     * Add service branding
-     * @param service {String} service name
-     */
-    self.addLogo = function(service) {
-      var html = '';
-      if (self.config.brands[service] && self.config.brands[service].logo) {
-        html += '<a href=\'' + self.config.brands[service].url + '\' class=\'' + service + '\'>';
-        html +=    '<img src="' + self.config.imagePath + self.config.brands[service].logo + '" />';
-        html += '</a>';
-        self.dom.modal_logo.html(html);
-      }
-    };
-
-    self.destroy = function() {
-      self.dom.form.each(function(index, elem) {
-        $(elem).off('submit');
-      });
-      self.dom.modal_overlay.off('click');
-      self.dom.btn_close.off('click');
-      self.dom.btn_next.off('click');
-      self.dom.btn_prev.off('click');
-      self.dom.container.remove();
-    };
-
-    /**
-     * Load template and register event handlers
-     * no param
-     */
-    self.init = function() {
-      $('body').append(template);
-      self.parseSelectors();
-      self.dom.modal_footer.show();
-      self.dom.form.each(function(index, elem) {
-        $(elem).on('submit', self.onSubmit);
-      });
-      self.dom.modal_overlay.on('click', self.close);
-      self.dom.btn_close.on('click', self.close);
-      self.dom.btn_next.on('click', self.nextPage);
-      self.dom.btn_prev.on('click', self.prevPage);
-    };
-
-    self.init();
-  };
-
-  var template = '<div id="u-search"><div class="modal"> <header class="modal-header" class="clearfix"><form id="u-search-modal-form" class="u-search-form" name="uSearchModalForm"> <input type="text" id="u-search-modal-input" class="u-search-input" /> <button type="submit" id="u-search-modal-btn-submit" class="u-search-btn-submit"> <span class="fas fa-search"></span> </button></form> <a class="btn-close"> <span class="fas fa-times"></span> </a><div class="modal-loading"><div class="modal-loading-bar"></div></div> </header> <main class="modal-body"><ul class="modal-results modal-ajax-content"></ul> </main> <footer class="modal-footer clearfix"><div class="modal-metadata modal-ajax-content"> <strong class="range"></strong> of <strong class="total"></strong></div><div class="modal-error"></div> <div class="logo"></div> <a class="nav btn-next modal-ajax-content"> <span class="text">NEXT</span> <span class="fal fa-chevron-right"></span> </a> <a class="nav btn-prev modal-ajax-content"> <span class="fal fa-chevron-left"></span> <span class="text">PREV</span> </a> </footer></div><div class="modal-overlay"></div></div>';
-})(jQuery);
-
-var AlgoliaSearch;
-(function($) {
-  'use strict';
-
-  /**
-   * Search by Algolia Search
-   * @param options : (object)
-   */
-  AlgoliaSearch = function(options) {
-    SearchService.apply(this, arguments);
-    var self = this;
-    var endpoint = 'https://' + self.config.appId + '-dsn.algolia.net/1/indexes/' + self.config.indexName;
-    self.addLogo('algolia');
-
-    /**
-     * Generate result list html
-     * @param data : (array) result items
-     */
-    self.buildResultList = function(data) {
-      var html = '';
-      $.each(data, function(index, row) {
-        var url = row.permalink || row.path || '';
-        if (!row.permalink && row.path) {
-          url = ROOT + url;
-        }
-        var title = row.title;
-        var digest = '';
-        html += self.buildResult(url, title, digest, index + 1);
-      });
-      html += '<script>try{pjax.refresh(document.querySelector(\'#u-search\'));document.addEventListener(\'pjax:send\',function(){$(\'#u-search\').fadeOut(500);$(\'body\').removeClass(\'modal-active\')});}catch(e){$(\'#u-search\').fadeOut(500);}</script>';
-      return html;
-    };
-
-    /**
-     * Generate metadata after a successful query
-     * @param data : (object) the raw search response data
-     */
-    self.buildMetadata = function(data) {
-      self.nav.current = data.page * data.hitsPerPage + 1;
-      self.nav.currentCount = data.hits.length;
-      self.nav.total = parseInt(data.nbHits);
-      self.dom.modal_metadata.children('.total').html(self.nav.total);
-      self.dom.modal_metadata.children('.range').html(self.nav.current + '-' + (self.nav.current + self.nav.currentCount - 1));
-      if (self.nav.total > 0) {
-        self.dom.modal_metadata.show();
-      } else {
-        self.dom.modal_metadata.hide();
-      }
-
-      if (data.page < data.nbPages - 1) {
-        self.nav.next = (data.page + 1) + 1;
-        self.dom.btn_next.show();
-      } else {
-        self.nav.next = -1;
-        self.dom.btn_next.hide();
-      }
-      if (data.page > 0) {
-        self.nav.prev = (data.page + 1) - 1;
-        self.dom.btn_prev.show();
-      } else {
-        self.nav.prev = -1;
-        self.dom.btn_prev.hide();
-      }
-    };
-
-    /**
-     * Send a GET request
-     * @param queryText : (string) the query text
-     * @param page : (int) the current page (start from 1)
-     * @param callback : (function)
-     */
-    self.query = function(queryText, page, callback) {
-      $.get(endpoint, {
-        query                     : queryText,
-        page                      : page - 1,
-        hitsPerPage               : self.config.per_page,
-        'x-algolia-application-id': self.config.appId,
-        'x-algolia-api-key'       : self.config.apiKey
-      }, function(data, status) {
-        if (status === 'success' && data.hits && data.hits.length > 0) {
-          var results = self.buildResultList(data.hits);
-          self.dom.modal_results.html(results);
-        } else {
-          self.onQueryError(queryText, status);
-        }
-        self.buildMetadata(data);
-        if (callback) {
-          callback(data);
-        }
-      });
-    };
-
-    return self;
-  };
-
-})(jQuery);
+  window.pjax && search.on('render', () => {
+    window.pjax.refresh(document.getElementById('algolia-hits'))
+  })
+})
